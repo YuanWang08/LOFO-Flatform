62e564fa9ac8992a76f5098c0796e02bd654af78
Wilson Wang <sckwilson1008@gmail.com>
2025-05-14 22:14:38 +0800

    refactor: 移除註解的使用者相關路由，整理路由結構

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -5,6 +5,20 @@ const userCtrl = require("../controllers/userController");
 
 router.get("/check", authmid.authRequired(), userCtrl.check);
 router.get("/info", authmid.authRequired(), userCtrl.getUserInfo);
-// router.put("/info", authmid.authRequired(), userCtrl.updateUserInfo);
+
+// register
+// router.post("/register", userCtrl.register);
+
+// login
+// router.post("/login", userCtrl.login);
+
+// reset password
+// router.post("/reset", userCtrl.resetPassword);
+
+// update password
+// router.put("/update", authmid.authRequired(), userCtrl.updatePassword);
+
+// update user info
+// router.put("/update", authmid.authRequired(), userCtrl.updateUserInfo);
 
 module.exports = router;
