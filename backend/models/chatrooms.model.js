ca77e9305221b39e1c5b721e91693cc6320eecd1
Wilson Wang <sckwilson1008@gmail.com>
2025-05-25 00:39:15 +0800

    feat: 移除chatrooms.model.js中的註解以簡化代碼

--- backend/models/chatrooms.model.js
+++ backend/models/chatrooms.model.js
@@ -1,4 +1,3 @@
-//chatrooms.model.js
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
@@ -25,8 +24,8 @@ module.exports = (sequelize) => {
       },
     },
     {
-      timestamps: true, // 啟用時間戳記
-      paranoid: true, // 啟用軟刪除
+      timestamps: true,
+      paranoid: true,
       tableName: "chatrooms",
     }
   );
