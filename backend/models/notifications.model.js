997fe7e3554b59c50bd0b0abfb2d9d3d1cc7fb08
Wilson Wang <sckwilson1008@gmail.com>
2025-05-25 00:39:39 +0800

    feat: 移除notifications.model.js中的註解以簡化代碼

--- backend/models/notifications.model.js
+++ backend/models/notifications.model.js
@@ -1,4 +1,3 @@
-// notifications.model.js
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
@@ -35,8 +34,8 @@ module.exports = (sequelize) => {
       },
     },
     {
-      timestamps: true, // 啟用時間戳記
-      paranoid: true, // 啟用軟刪除
+      timestamps: true,
+      paranoid: true,
       tableName: "notifications",
     }
   );
