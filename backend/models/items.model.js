1a6245d3ff596c5e4a8cea29dbb797d1f0163cbe
Wilson Wang <sckwilson1008@gmail.com>
2025-05-25 00:39:30 +0800

    feat: 移除items.model.js中的註解以簡化代碼

--- backend/models/items.model.js
+++ backend/models/items.model.js
@@ -1,4 +1,3 @@
-// items.model.js
 const e = require("express");
 const { DataTypes } = require("sequelize");
 
@@ -99,8 +98,8 @@ module.exports = (sequelize) => {
       },
     },
     {
-      timestamps: true, // 啟用時間戳記
-      paranoid: true, // 啟用軟刪除
+      timestamps: true,
+      paranoid: true,
       tableName: "items",
     }
   );
